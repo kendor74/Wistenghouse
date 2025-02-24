c1177fea3eea8513bdaf3813e6c6f9120ae2f83d
kendor <103393540+kendor74@users.noreply.github.com>
2025-02-24 15:01:14 +0200

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,1 +1,91 @@
-const e=e=>console.log(e),t=e=>document.querySelector(e),o=e=>document.querySelectorAll(e),n=document.documentElement,c=e=>n.classList.toggle("debug"),d=e=>{n.getAttribute("data-theme")&&"light"!=n.getAttribute("data-theme")?n.setAttribute("data-theme","light"):n.setAttribute("data-theme","dark")},r=e=>document.body.style.overflow="hidden",s=e=>document.body.style.overflow="",a=e=>{document.querySelector(".pop-form-bg").classList.remove("visible"),s()};document.getElementById("pop-form-open-btn").addEventListener("click",(function(e){e.preventDefault(),document.querySelector(".pop-form-bg").classList.add("visible"),r()})),document.querySelector(".pop-form-bg").addEventListener("click",(function(e){e.target.classList.contains("pop-form-bg")&&(e.preventDefault(),this.classList.remove("visible"),s())}));const l=new IntersectionObserver((e=>{e.forEach((e=>{e.isIntersecting&&e.target.classList.add("show")}))})),i=document.querySelectorAll(".hidden");i.forEach((e=>l.observe(e)));let u=0;const m=document.querySelectorAll(".quote"),h=document.querySelector(".quote-wrapper"),v=document.querySelectorAll(".dot");v[0].classList.add("active");let f=0,g=0,b=!1;function y(){h.style.transform=`translateX(${100*u}%)`,v.forEach((e=>e.classList.remove("active"))),v[u].classList.add("active")}function nextQuote(){u=(u-1+m.length)%m.length,y(),L()}function prevQuote(){u=(u+1)%m.length,y(),L()}function goToQuote(e){u=e,y(),L()}let p=setInterval(nextQuote,5e3);function L(){clearInterval(p),p=setInterval(nextQuote,5e3)}h.addEventListener("touchstart",(e=>{f=e.touches[0].clientX,b=!0,h.style.transition="none"})),h.addEventListener("touchmove",(e=>{b&&(g=e.touches[0].clientX)})),h.addEventListener("touchend",(()=>{if(!b)return;b=!1,h.style.transition="transform 0.3s ease-in-out";const e=f-g;e>50?nextQuote():e<-50&&prevQuote()}));let w=0;const S=document.querySelector(".dynamic-word"),q=e=>{w=(w+1)%textSlider.wordList.length,S.textContent=textSlider.wordList[w]};setInterval(q,textSlider.Interval),q(),window.matchMedia&&window.matchMedia("(prefers-color-scheme: dark)").matches?n.setAttribute("data-theme","dark"):n.setAttribute("data-theme","light"),window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change",(e=>{const t=e.matches?"dark":"light";n.setAttribute("data-theme",t)}));
+const e = (e) => console.log(e),
+  t = (e) => document.querySelector(e),
+  o = (e) => document.querySelectorAll(e),
+  n = document.documentElement,
+  c = (e) => n.classList.toggle("debug"),
+  d = (e) => {
+    n.getAttribute("data-theme") && "light" != n.getAttribute("data-theme")
+      ? n.setAttribute("data-theme", "light")
+      : n.setAttribute("data-theme", "dark");
+  },
+  r = { wordList: ["ثلاجات", "غسالات","فريزر","تاكيفات"], Interval: 3e3 },
+  s = (e) => (document.body.style.overflow = "hidden"),
+  a = (e) => (document.body.style.overflow = ""),
+  l = (e) => {
+    document.querySelector(".pop-form-bg").classList.remove("visible"), a();
+  };
+document
+  .getElementById("pop-form-open-btn")
+  .addEventListener("click", function (e) {
+    e.preventDefault(),
+      document.querySelector(".pop-form-bg").classList.add("visible"),
+      s();
+  }),
+  document
+    .querySelector(".pop-form-bg")
+    .addEventListener("click", function (e) {
+      e.target.classList.contains("pop-form-bg") &&
+        (e.preventDefault(), this.classList.remove("visible"), a());
+    });
+const i = new IntersectionObserver((e) => {
+    e.forEach((e) => {
+      e.isIntersecting && e.target.classList.add("show");
+    });
+  }),
+  u = document.querySelectorAll(".hidden");
+u.forEach((e) => i.observe(e));
+let m = 0;
+const h = document.querySelectorAll(".quote"),
+  v = document.querySelector(".quote-wrapper"),
+  f = document.querySelectorAll(".dot");
+f[0].classList.add("active");
+let g = 0,
+  b = 0,
+  y = !1;
+function L() {
+  (v.style.transform = `translateX(${100 * m}%)`),
+    f.forEach((e) => e.classList.remove("active")),
+    f[m].classList.add("active");
+}
+function nextQuote() {
+  (m = (m - 1 + h.length) % h.length), L(), w();
+}
+function prevQuote() {
+  (m = (m + 1) % h.length), L(), w();
+}
+function goToQuote(e) {
+  (m = e), L(), w();
+}
+let p = setInterval(nextQuote, 5e3);
+function w() {
+  clearInterval(p), (p = setInterval(nextQuote, 5e3));
+}
+v.addEventListener("touchstart", (e) => {
+  (g = e.touches[0].clientX), (y = !0), (v.style.transition = "none");
+}),
+  v.addEventListener("touchmove", (e) => {
+    y && (b = e.touches[0].clientX);
+  }),
+  v.addEventListener("touchend", () => {
+    if (!y) return;
+    (y = !1), (v.style.transition = "transform 0.3s ease-in-out");
+    const e = g - b;
+    e > 50 ? nextQuote() : e < -50 && prevQuote();
+  });
+let q = 0;
+const A = document.querySelector(".dynamic-word"),
+  E = (e) => {
+    (q = (q + 1) % r.wordList.length), (A.textContent = r.wordList[q]);
+  };
+setInterval(E, r.Interval),
+  E(),
+  window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches
+    ? n.setAttribute("data-theme", "dark")
+    : n.setAttribute("data-theme", "light"),
+  window
+    .matchMedia("(prefers-color-scheme: dark)")
+    .addEventListener("change", (e) => {
+      const t = e.matches ? "dark" : "light";
+      n.setAttribute("data-theme", t);
+    });
+//# sourceMappingURL=maps/scripts.js.map
